181741f1e99c27659a501cf55faa540e8fda4ad1
Scott W Locke <scott.w.locke@gmail.com>
2024-05-09 01:48:47 +0000

    removed config-git.sh script from devcontainer.json failing

--- .devcontainer/configure-git.sh
+++ .devcontainer/configure-git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo git config --system --add safe.directory "${containerWorkspaceFolder}"
+sudo git config --global --add safe.directory /workspaces/puppet_devcontainer
 sudo git config core.ignorecase false
-sudo git config core.autocrlf true
+sudo git config core.autocrlf false
